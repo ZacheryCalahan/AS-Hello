069f5820675d7a163f6f66bba9acc3e75b23b385
zacherycalahan33@gmail.com <ZachRocks12>
2020-01-29 07:26:02 -0500

    First commit

---
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.hello;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity<Button> extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
